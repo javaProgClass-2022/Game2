92aef180269d9425da7bdb3ada4ac30389eae476
DeusAdAstra <120408251+DeusAdAstra@users.noreply.github.com>
2023-01-18 13:29:13 -0500

    Fixed bad Merging
    stupid github stop deleting files. Stupid stupid dumb dumb.

---
+++ src/game/Bullet.java
@@ -0,0 +1,34 @@
+package game;
+
+public class Bullet {
+	int startX, startY;
+	int dirX, dirY;
+	int width = 2, height = 2;
+	
+	double speed = 6;
+	
+	Bullet(int startX, int startY, int targetX, int targetY) {
+		this.startX = startX;
+		this.startY = startY;
+		
+		double vx = targetX - startX;
+        double vy = targetY - startY;
+
+        double distance = (int)Math.sqrt(vx * vx + vy * vy);
+
+        this.dirX = (int)((vx / distance) * speed);
+        this.dirY = (int)((vy / distance) * speed);
+        
+//        System.out.println(vx);
+//		System.out.println(vy);
+//		System.out.println(distance);
+//		System.out.println();
+        
+	}
+	
+	void move() {
+		
+		startX += dirX;
+        startY += dirY;
+	}
+}
